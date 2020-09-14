e68094b82667c01c62ed293b6ac037122a103a1a
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-09-14 00:17:09 -0400

    I configured application.properties to work with H2 and MySQL/MariaDB and to create the table and database from scratch if needed.

--- src/main/java/com/perscholas/casestudy/repositories/UserRepository.java
+++ src/main/java/com/perscholas/casestudy/repositories/UserRepository.java
@@ -7,4 +7,16 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    //sample custom query
+//    @Query("select u from User u where u.firstname like %?1")
+//    List<User> findByFirstnameEndsWith(String firstname);
+
+    // Using named parameters
+//    @Query("select u from User u where u.firstname = :firstname or u.lastname = :lastname")
+//    User findByLastnameOrFirstname(@Param("lastname") String lastname,
+//                                   @Param("firstname") String firstname);
+
+//    Query("select u from #{#entityName} u where u.lastname = ?1")
+//    List<User> findByLastname(String lastname);
 }
